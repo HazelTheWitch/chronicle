ef1cc7c2bd5ed1ced31432fa7c3522f6a369efe1
Hazel Rella <hazelrella11@gmail.com>
2025-01-09 17:55:47 -0800

    cli framework

--- chronicle-cli/src/args.rs
+++ chronicle-cli/src/args.rs
@@ -1,15 +1,13 @@
 use std::path::PathBuf;
 
-use chronicle::record::RecordDetails;
-use clap::{Args, Parser, Subcommand};
-use directories::ProjectDirs;
-use lazy_static::lazy_static;
+use chronicle::{author::AuthorQuery, record::RecordDetails, search::Query, tag::TagExpression};
+use clap::{Args, Parser, Subcommand, ValueEnum};
 use tracing::Level;
 use url::Url;
 
 use crate::PROJECT_DIRS;
 
-#[derive(Parser)]
+#[derive(Debug, Parser)]
 #[command(version, about, long_about = None)]
 pub struct Arguments {
     #[arg(short, long, env = "CHRONICLE_LOG", default_value_t = Level::INFO)]
@@ -19,54 +17,152 @@ pub struct Arguments {
     #[arg(short, long, default_value = PROJECT_DIRS.config_dir().join("config.toml").into_os_string())]
     pub config: PathBuf,
     #[command(subcommand)]
-    pub command: Option<Command>,
+    pub command: Command,
 }
 
-#[derive(Subcommand)]
+#[derive(Debug, Subcommand)]
 pub enum Command {
-    Search {
-        #[arg(short, long)]
-        destination: Option<PathBuf>,
-        query: String,
+    /// Operations with works
+    Work {
+        #[command(subcommand)]
+        command: WorkCommand,
     },
-    List,
-    Add {
-        /// The path of the work.
-        path: PathBuf,
-        /// Copy the work to the data directory if not already there.
-        #[arg(short, long)]
-        copy: bool,
-        #[command(flatten)]
-        details: WorkDetails,
+    /// Operations with tags
+    Tag {
+        #[command(subcommand)]
+        command: TagCommand,
     },
-    Import {
-        /// The link to the work.
-        link: String,
-        /// Skip duplicate check.
-        #[arg(short, long)]
-        force: bool,
-        #[command(flatten)]
-        details: WorkDetails,
+    /// Operations with authors
+    #[command(alias = "artist")]
+    Author {
+        #[command(subcommand)]
+        command: AuthorCommand,
     },
+    /// List and authenticate importers
     Service {
         #[command(subcommand)]
         command: ServiceCommand,
     },
-    WriteConfig,
-    Tag {
-        #[arg(short, long)]
-        targets: Vec<String>,
-        tags: Vec<String>,
+}
+
+#[derive(Debug, Subcommand)]
+pub enum WorkCommand {
+    /// Chronicle a local file as a work
+    Add {
+        /// The local path to the file
+        path: PathBuf,
+        #[command(flatten)]
+        details: WorkDetails,
+    },
+    /// Chronicle a link as work(s)
+    Import {
+        /// The source of the work(s)
+        source: Url,
+        #[command(flatten)]
+        details: WorkDetails,
+    },
+    /// Search and display works
+    Search {
+        #[command(flatten)]
+        display_options: WorkDisplayOptions,
+        /// The query to search for, if omitted the query is read from stdin
+        query: Option<Query>,
+    },
+    /// List all works chronicled
+    List {
+        #[command(flatten)]
+        display_options: WorkDisplayOptions,
     },
 }
 
-#[derive(Subcommand)]
+#[derive(Debug, Subcommand)]
+pub enum TagCommand {
+    /// Tag a set of works with tags
+    Work {
+        /// The search query to tag
+        query: String,
+        /// The tags to apply to each work in the query
+        tags: Vec<String>,
+    },
+    /// Tag a set of tags
+    Meta {
+        /// The expression to perform tagging with, if omitted the expression is read from stdin
+        ///
+        /// Examples:
+        /// - subtag/supertag
+        /// - subtag/(sibling,tags)/supertag
+        expression: Option<TagExpression>,
+    },
+}
+
+#[derive(Debug, Subcommand)]
+pub enum AuthorCommand {
+    /// List all authors
+    List {
+        #[command(flatten)]
+        display_options: AuthorDisplayOptions,
+    },
+    /// Assign an alias to an author
+    Alias {
+        /// The author to alias
+        ///
+        /// Can be a name, id, or url
+        query: AuthorQuery,
+        /// The alias to assgn to the author
+        alias: String,
+    },
+    /// Assig a url to an author
+    AddUrl {
+        /// The author to assign the url to
+        ///
+        /// Can be a name, id, or url
+        query: AuthorQuery,
+    },
+}
+
+#[derive(Debug, Subcommand)]
 pub enum ServiceCommand {
-    Login { service: String },
+    /// Login to a service
+    Login {
+        /// The service to log into, if omitted will prompt for each service in sequence
+        service: Option<String>,
+    },
+    /// List all services available
     List,
 }
 
-#[derive(Default, Clone, Args)]
+#[derive(Debug, Args)]
+pub struct WorkDisplayOptions {
+    /// Specifies which columns to display
+    #[arg(short, long, value_enum, default_values_t = vec![WorkColumn::Id, WorkColumn::Path])]
+    pub columns: Vec<WorkColumn>,
+}
+
+#[derive(Debug, Args)]
+pub struct AuthorDisplayOptions {
+    /// Specifies which columns to display
+    #[arg(short, long, value_enum, default_values_t = vec![AuthorColumn::Id, AuthorColumn::Aliases])]
+    pub columns: Vec<AuthorColumn>,
+}
+
+#[derive(Debug, Clone, Copy, ValueEnum)]
+pub enum AuthorColumn {
+    Id,
+    Aliases,
+    Urls,
+}
+
+#[derive(Debug, Clone, Copy, ValueEnum)]
+pub enum WorkColumn {
+    Id,
+    Path,
+    Hash,
+    Title,
+    AuthorId,
+    Caption,
+}
+
+#[derive(Debug, Default, Clone, Args)]
 pub struct WorkDetails {
     /// A list of tags to associate with the work.
     pub tags: Vec<String>,
